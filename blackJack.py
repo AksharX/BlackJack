71d97055d37a593c8b08a74e249b7d2585b58bb3
Akshar Patel <Akshar.Patel@us.na.ey.net>
2018-06-06 16:41:29 -0400

    Added minor bug Fixes

--- blackJack.py
+++ blackJack.py
@@ -2,13 +2,12 @@ from deck import Deck_Generator
 from pprint import pprint
 from tabulate import tabulate
 
-deck_Gen = Deck_Generator(4)
-deck = deck_Gen.deck
 
 class Hand:
     hand = {}
     def __init__(self):
         pass
+    
     def addCard(self,card,handNum):
         if handNum not in self.hand:
             self.hand[handNum] = {
@@ -18,15 +17,12 @@ class Hand:
         else:
             self.hand[handNum]["Cards"].append(card)
             self.hand[handNum]["Hand_Total"] = self.calculateCardTotal(self.hand[handNum]["Cards"])
+    
     def __str__(self):
         handList = []
         for h in self.hand:
             handList.append("\n".join(str(c) for c in h["Cards"]))
         return tabulate(handList)
-        
-        
-            
-
             
     def calculateCardTotal(self,cards):
         total = 0
@@ -41,19 +37,18 @@ class Hand:
             for x in range(Aces_Count):
                 total -= 10
     
-
-
 class BasePlayer:
     hand = None
     deck = None
-    def __init__(self,__deck):
+    hit_Command = None
+    def __init__(self,__deck,__commands):
         self.deck = __deck
         self.hand = Hand()
+        self.hit_Command = __commands["Hit"]
+    
     def hit(self,handNum):
-        c = self.deck.deal_card()
-        if c == None:
-            self.deck.shuffle()
-        self.hand.addCard(c,handNum)
+        self.hit_Command.execute(Hand,handNum)
+
     def stay(self):
         pass
 
@@ -62,35 +57,68 @@ class BasePlayer:
 
 class Dealer(BasePlayer):
 
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self,__deck,__commands):
+        super().__init__(__deck,__commands)
 
 class Player(BasePlayer):
-    def __init__(self):
-        pass
-
+    auto = None
+    def __init__(self,__deck,__commands):
+        super().__init__(__deck,__commands)
+    
 
 class commands():
     pass
-    def execute(self):
-        pass
 
-# class hit_Command(commands):
-#     basePlayer = None 
-#     deck = None
-#     def __init__(self,__basePlayer,__deck):
-#         self.basePlayer = __basePlayer
-#         self.deck = __deck
+class hit_Command(commands):
+    basePlayer = None 
+    deck = None
+    def __init__(self,__deck):
+        self.deck = __deck
     
-#     def execute(self):
-#         c = deck.deal_card
+    def execute(self,hand,handNum):
         
-#         if c == None:
-#             deck.shuffle()
-             
-#         self.basePlayer.addCard(c)
+        c = self.deck.deal_card()
+        
+        if c == None:
+            self.deck.shuffle()
+        
+        hand.addCard(c,handNum)
+
+class BlackJack:
+    players = []
+    dealer = None
+    deck = None
+
+    def __init__(self):
+        pass
+    def CreateDeck(self,num_Decks):
+        deckGen= Deck_Generator(num_Decks)
+        self.deck = deckGen.deck
 
 
+    def addPlayers(auto = false)
+        
+        if auto == False:
+            hit = hit_Command(self.deck)
+            self.dealer = Dealer(self.deck,hit)
+
+            for i in range(num_Players):
+                self.players.append(Player(self.deck,hit))
     
 
+    def playGame(self,rounds=1):
+
+        #Initial Setup - Hand Everybody two Cards!
+        for i in range(rounds):
+            everyOne = self.players + [self.dealer]
+            for people in everyOne * 2:
+                people.hit(1)
+
+            for people in self.players:
+                = input("Hit or Stay or Double")
+
+
+                    
+
+            
 
---
+++ main.py
@@ -0,0 +1,14 @@
+import BlackJack from blackJack
+
+
+def main():
+    
+    game = BlackJack(__deck,NumPlayers)
+
+    
+
+    
+
+
+if __name__ == "__main__":
+    main()
